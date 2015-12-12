6216edf44c3fb86c94c68077fd1620c8a77da2b3
Aleksey Malyshev <aleksey.malyshev@mail.ru>
2015-12-12 17:47:45 +0300

    fixed authentication header

--- config/auth.js
+++ config/auth.js
@@ -28,6 +28,6 @@ module.exports.enableCORS = (req, res, next) => {
   res.header('Access-Control-Expose-Headers', 'Authenticate');
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Authenticate, Accept');
   next();
 }
